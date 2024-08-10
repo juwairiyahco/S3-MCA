d2167ab0e9eba1f5f7e3c6a534befcc7d013728f
Vyshnav <vyshnavmohan20@gmail.com>
2024-08-10 21:40:35 +0530

    10/8

--- MAD/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ MAD/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -13,14 +13,15 @@ public class MainActivity extends AppCompatActivity
 {
     EditText t1;
     Button bac;
+    String firstNumber="",operator="";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         t1=(EditText) findViewById(R.id.t1);
         bac=(Button) findViewById(R.id.bac);
-        int b[]=new int[11];
-        for(int i=0;i<=10;i++)
+        int b[]=new int[17];
+        for(int i=0;i<=16;i++)
         {
             String s="b"+(i);
             int rid=getResources().getIdentifier(s,"id",getPackageName());
@@ -49,11 +50,50 @@ public class MainActivity extends AppCompatActivity
     }
 
     private void handleButtonClick(int id) {
-            String txt=t1.getText().toString();
+        String txt = t1.getText().toString();
 
-            t1.setText(txt+((Button)findViewById(id)).getText());
+        if (id == R.id.b16) {
+            if (!firstNumber.isEmpty() && !operator.isEmpty()) {
+                String secondNumber = txt;
+                double result = 0;
+                double num1 = Double.parseDouble(firstNumber);
+                double num2 = Double.parseDouble(secondNumber);
 
+                switch (operator) {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "x":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        break;
+                }
+                t1.setText(String.valueOf(result));
+                firstNumber = "";
+                operator = "";
+            }
+        } else if (id == R.id.b10) {
+            if (!txt.contains(".")) {
+                t1.setText(txt + ((Button) findViewById(id)).getText());
+            }
+        } else if (id == R.id.b12 || id == R.id.b13 || id == R.id.b14 || id == R.id.b15) {
+            if (!txt.isEmpty()) {
+                firstNumber = txt;
+                operator = ((Button) findViewById(id)).getText().toString();
+                t1.setText("");
+            }
+        } else if (id == R.id.b11) {
+            if (txt.length() > 0) {
+                t1.setText(txt.substring(0, txt.length() - 1));
+            }
+        } else {
+            t1.setText(txt + ((Button) findViewById(id)).getText());
+        }
     }
-
 
 }
