67752337349dd6dd1325017705002f29f8b5e486
juwairiyahco <84274172+juwairiyahco@users.noreply.github.com>
2024-09-06 15:13:34 +0530

    6/9

---
+++ MAD/ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    String arr[]={"Juwairiyah","Mishal","Vyshnav"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv=(ListView) findViewById(R.id.lv);
+        ArrayAdapter adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arr);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,"I AM "+adapter.getItem(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
